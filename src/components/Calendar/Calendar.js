18770e6d0b4a140e543e0150bdbb97a50ff793e1
Alex Dodd <alex.dodd@cmx245.local>
2020-02-10 12:31:28 -0500

    room numbers customized

--- src/components/Calendar/Calendar.js
+++ src/components/Calendar/Calendar.js
@@ -1,58 +1,113 @@
 import React, { useState } from 'react';
-import styled from 'styled-components';
-import { RoomCalculator, RoomCheck } from '../../utils/RoomCalculator'
-
-const Cal = styled.div`
-	display: flex;
-	flex-wrap: wrap;
-`;
-	
-const Day = styled.div`
-	width: calc(14% - 100px);
-	margin: 50px; 
-`;
-
-const Room = styled.p`
-	font-weight: bold;
-`;
+import {
+	Cal,
+	Day,
+	Room,
+	Label
+} from './Calendar.styles';
 
 const Calendar = () => {
-	const numOfRooms = 25;
-	const numOfRoomsArray = Array.from(Array(numOfRooms + 1).keys());
-	const numOfDays = 7;
+	const roomsArray = [
+		227,
+		228,
+		229,
+		230,
+		231,
+		232,
+		233,
+		234,
+		235,
+		236,
+		237,
+		238,
+		239,
+		242,
+		243,
+		244,
+		245,
+		246,
+		247,
+		248,
+		249,
+		250,
+		251,
+		252,
+		253
+	];
+	let uncheckedRooms = [...roomsArray];
+	const [numOfDays, setNumOfDays] = useState(null);
 	const numOfDaysArray = Array.from(Array(numOfDays).keys());
-	const [uncheckedRooms, updateRooms] = useState(numOfRoomsArray);
+	const [numOfChecks, setNumOfChecks] = useState(null);
+	const [isDisabled, changeIsDisabled] = useState(false);
+	const randomNumber = (num) => Math.floor(Math.random() * num);
+
+	const RoomCheck = (dailyChecks) => {
+		if (!dailyChecks.length) {
+			for (let i = 0; i < numOfChecks; i++) {
+				const rand = randomNumber(uncheckedRooms.length)
+				const removedNum = uncheckedRooms.splice(rand, 1)[0];
+				dailyChecks.push(removedNum);
+			}
+		} else {
+			for (let i = 0; i < numOfChecks; i++) {
+				const rand = randomNumber(uncheckedRooms.length)
+				const removedNum = uncheckedRooms.splice(rand, 1)[0];
+				dailyChecks.push(removedNum);
+				if (dailyChecks.length === numOfChecks) return;
+			}
+		}
+	};
 
 	const RoomCalculator = (i) => {
-		if (uncheckedRooms.length <= 3 || numOfDays === i + 1) {
-			const allRooms = uncheckedRooms.splice(1, 25);
-			uncheckedRooms.pop();
-			// updateRooms(numOfRoomsArray);
-			return allRooms
-		}
-		const randomNumber = (num) => Math.floor(Math.random() * num + 1);
-		const numOfChecks = randomNumber(6);
-		return uncheckedRooms.reduce((acc, _, i) => {
-			if (i < numOfChecks) {
-				const removedNum = uncheckedRooms.splice(randomNumber(uncheckedRooms.length - 1), 1)[0];
-				if (removedNum) acc.push(removedNum);
+		console.log('Day', i + 1)
+		const dailyChecks = [];
+		if (uncheckedRooms.length <= numOfChecks) {
+			for (let i = 0; i < numOfChecks; i++) {
+				if (uncheckedRooms.length) dailyChecks.push(uncheckedRooms.pop());
+			};
+			if (dailyChecks.length < numOfChecks) {
+				uncheckedRooms = [...roomsArray];
+				RoomCheck(dailyChecks);
 			}
-			return acc
-		}, []);
+		} else {
+			RoomCheck(dailyChecks);
+		}
+		return dailyChecks;
+	};
+
+	const handleChecksOnChange = (e) => {
+		e.preventDefault();
+		const input = Number(e.target.value)
+		if (!input && e.target.value !== '') {
+			alert('value must be a number')
+			e.target.value = '';
+		}
+		setNumOfChecks(input)
+		for (let i = input; i < 100; i++) { 
+			if (i % input === 0 && (i > uncheckedRooms.length || i * input === uncheckedRooms.length)) {
+				setNumOfDays(i);
+				changeIsDisabled(!isDisabled);
+				return;
+			}
+		}
 	};
 
 	return (
-		<Cal>
-			{numOfDaysArray.map((day, i) => {
-				return (
-				<Day key={`Day ${i}`}>
-					<h3>Day {i + 1}</h3>
-						{RoomCalculator(i).map((room, j) => (
-						<Room key={`Day ${i} Room ${j}`}>Room {room}</Room>
-						))}
-				</Day>
-			)})}
-		</Cal>
+		<>
+			<h1>Room Check Planner</h1>
+			<Label htmlFor="numOfChecks">How many rooms per day would you like to schedule?</Label>
+			<input id="numOfChecks" onChange={(e)=> handleChecksOnChange(e)} placeholder="3" disabled={isDisabled}></input>
+			<Cal>
+				{numOfChecks && numOfDays &&
+				numOfDaysArray.map((day, i) => {
+					return (
+					<Day key={`Day ${i}`}>
+						<h3>Day {i + 1}</h3>
+							{RoomCalculator(i).map((room, j) => <Room key={`Day ${i} Room ${j}`}>Room {room}</Room>)}
+					</Day>
+				)})}
+			</Cal>
+		</>
 		);
 };
 
---
+++ src/components/Calendar/Calendar.styles.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const Cal = styled.div`
+	display: flex;
+	flex-wrap: wrap;
+`;
+	
+export const Day = styled.div`
+	width: calc(14% - 100px);
+	margin: 50px; 
+`;
+
+export const Room = styled.p`
+	font-weight: bold;
+`;
+
+export const Label = styled.label`
+  margin-right: 20px;
+`;
